28b55be6f955f28214823defaf57b36d86f154b2
VovaKorn30 <94910425+VovaKorn30@users.noreply.github.com>
2025-12-06 22:10:30 +0200

    Enhance CLI say command with output formatting
    Updated the say command to format output and added docstrings.

--- cli_tool.py
+++ cli_tool.py
@@ -2,16 +2,18 @@ import click
 
 @click.group()
 def cli():
+    """Група команд CLI"""
     pass
 
 
 @cli.command()
 @click.option("--name", prompt="Введіть ім'я", help="Ім'я користувача для виводу")
 def say(name):
+    """Команда для виводу імені, крім тих, що починаються на 'p'"""
     if name.lower().startswith('p'):
         click.echo("Ім’я не підходить")
     else:
-        click.echo(name)
+        click.echo(f"Введене ім'я: {name}")
 
 
 if __name__ == "__main__":
